9b2d9db8e0455d0e076e0e5efffb6a2c9720c8b4
Scott Meade <scott@synapsoftware.com>
2020-12-27 14:24:31 -0700

    Demonstrate Turbo Streams updates

---
+++ app/controllers/hurrahs_controller.rb
@@ -0,0 +1,22 @@
+class HurrahsController < ApplicationController
+  before_action :set_room, :set_message
+
+  def create
+    @message.update! hurrah_count: @message.hurrah_count + 1
+
+    respond_to do |format|
+      format.turbo_stream { }
+      format.html { redirect_to @room }
+    end
+  end
+
+  private
+    def set_room
+      @room = Room.find(params[:room_id])
+    end
+
+    def set_message
+      @message = @room.messages.find(params[:message_id])
+    end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_20_113435) do
+ActiveRecord::Schema.define(version: 2020_12_27_211613) do
 
   create_table "messages", force: :cascade do |t|
     t.integer "room_id", null: false
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "hurrah_count", default: 0
     t.index ["room_id"], name: "index_messages_on_room_id"
   end
 
